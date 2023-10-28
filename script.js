9f07ee4943df4608784c54e41559c035e4515eb4
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2023-10-28 13:19:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,206 @@
-@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@100&family=Nothing+You+Could+Do&family=Nunito:wght@800&family=Sometype+Mono&family=Tangerine&display=swap');
+const messageBoxes = [
+    {
+        textId: 'storyText',
+        messages: [ 
+        "We were at a new house and finished moving boxes. I got tired from moving in furniture and wet upstairs to rest.",
+        "  I saw a bag with all your underwear and took one out. After looking at it and thinking about you ,",
+         "I got hard and couldn't get it to go down. U came up looking for me I there ur underwear down and pretended like nth happened.",
+         " U saw ur underwear on the floor. Reached down for it then as u were getting up u lose ur balance and fell on my lap.",
+        " My dick didn't go into ur pussy but it went straight between ur thighs.",
+         "U realized then got up and closed all the windows and blinds looked at me disappointed the sat back down in the same position. ",
+         "U started to play with it and it got sensitive. U removed your shorts and rubbed ur pussy against my veins until you got wet.",
+        " I got impatient held onto you while u sat on my lap and pushed it inside you. You let out a crying moan. ",
+        "I began to thrust my hips up and down as u bounced on my dick. I slid my hands under your shirt and started to play with your nipples.",
+         "I pulled out and came all over ur back as we laid there. We then took a bath and went back to moving boxes and furniture. ",       
+       
+         "end-",
+        "Love in The House ",
 
-@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@100&family=Macondo&family=Nothing+You+Could+Do&family=Nunito:wght@800&family=Sometype+Mono&family=Tangerine&display=swap');
+            // Add more messages here
+        ],
+        currentMessageIndex: 0
 
-
-@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@100&family=Macondo&family=Macondo+Swash+Caps&family=Nothing+You+Could+Do&family=Nunito:wght@800&family=Sometype+Mono&family=Tangerine&display=swap');
-*
+        
+    },
+    
 {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Sometype Mono', monospace;
-}
-    :root
-{
-    --clr: #fff;
-}
-body {
-    font-family: 'Macondo', cursive;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    background: linear-gradient(180deg, rgb(255, 169, 169) 0%, rgb(255, 130, 157) 35%, rgb(255, 76, 91) 100%);
+        textId: 'storyText2',
+        messages: [
+           " Só you were in the kitchen. Making lunch for us. U only had an apron on. While in the apron the side of your boobs slowty slipped out making you very sexy. I walked out to find a pen and I saw you. U bent over and your pussy was directly in my view. I slowly walked up on you and slapped your ass. ",
+           "You got shocked and asked what I was doing but before you could finish u kissed you. Our tongues twirled with each other. U became very silent afterwards. I pulled my pants down and reached for between your legs. U began to stroke my dick and I slowly be an rubbing your pussy. We went faster and faster until we came. ",
+           "We became exhausted but still continued. I lift you up brought you to the bathroom and ripped your apron off showing your grand breasts. I slowly pushed your down and made you suck on my dick. I came and u wanted to get pull off but I held down and came down ur throat. U came off and u had a wanting and loving look on your face. ",
+           "I picked u up hled your arms back and shoved my dick deep inside your body. U began to moan in a soft voice and I held your mouth while I continued to pound you. I turned you towards the wall and continued to pound you. You begged for more and I slapped your ass. As I came inside u nd pulled out your legs be an shivering.",
+           " U fell on your knees and asked for more respectfully. I sat down and you sat on my dick with our chests facing each other. U began to ride me u bit into my shoulder softly, I hugged and gripped onto me and u continued to ride me. Your tongue slowly rubbed over my body as I bit and sucked onto the side of your boobs.",
+           " You released another soft moan as my tongue made it's way to your nipples. As I continued sucking and your kept riding with your shaking legs we both came at once and took a breather as your breast was still in my mouth and my dick was still inside you. I stood up with u still gripped  to me and my dick still inside you and walked to bed.",
+           " We agreed to changing the sheets after we get back up and covered ourselves with blankets. We coddles each other my dick slowly throbbing inside your body. The sun set and your breasts developed my face. Your nipples got hard and my dick got harder. I sucked on your tits gently and u slowly moved around to ride my dick thoroughly."  ,
+           " We continued until we both fell asleep. My dick slipped out while we slept and you grabbed it and pushed it back in.",
+            
+            "end.",
+            "Kitchen Madness -new-",
+            "",
+            "",
+            "",
+            
+            // Add more messages here
+        ],
+        currentMessageIndex: 0
+    },
+
+    {
+        textId: 'storyText3',
+        messages: [
+            "We're both under the blanket cuddled up",
+            ", together trying to get as warm as possible ",
+             ",from skin contact. Under the blanket ",
+              "your ass was slowly rubbed against against  ",
+               "my dick but we ignored it because we were  ",
+               " too focused on the cold. ",
+                " U slipped your hands down my pants , ",
+              "and found it very warm. So I did the same. ",
+               " I then made my way below your shirt and  ",
+               "held your boobs. We both had an idea",
+               " of how to get warmer much faster but ",
+               "it was risky. We grabbed a second blanket ",
+               "removed all our clothes and embraced each ",
+               "other while naked. We both felt at peace. ",
+               "I got a oner and it rubbed against your legs.",
+               " The more u shifted around the harder and hotter it got. ",
+               "I tried to make an apology but",
+               " you stuffed on your your tits in my",
+               " mouth and told me to hush. ",
+               "You grabbed my hot throbbing dick and",
+               " stuck it deep inside you.",
+               " You began moving your hips slowly ",
+               "as we were still clinging onto each other.",
+               " I took action and gripped your hips then ",
+               "began thrusting. You asked what I was doing ",
+               "and I sucked on your tits and began thrusting ",
+               "deeper and slower hitting a spot then made you instantly get wet. ",
+               "You began to moan louder and louder but the blankets",
+               " muffled the noise. U got my off your tits but my dick remained inside of you.",
+               " You kissed my and out tongues made contact with each other for a long ",
+               "time while u still grinded on my dick. We were getting hotter. ",
+               "I made my way on top of you and slowly rubbed my dick between ",
+               "your beautiful legs your nails were planted deep ",
+               "into my back leaving scratches in every area.",
+               " U then made ur way o top of me and began grinding until I came.",
+               " We pulled out and I came all over your breasts.",
+               " U began licking the cum off. ",
+               "Ur chests connected and our nipple rubbed against each other" ,
+               ",we began sweating from how hot it was getting and ",
+               "decided to remove the blankets all together. ",
+               "The cold was no longer a problem for us in the moment.",
+               " U dragged you to the edge of the bed and flipped you over.",
+               "I held your arms behind your back and be an sucking on your neck.",
+               " I slowly Inserted my dick again and started to thrust",
+               "My balls started slapping against your pussy",
+               " with each push leave a sensitive sensation.",
+               " You couldn't take anymore but yet you wanted",
+               " more at the same time.",
+               " Each moan you'd make I'd slap your ass it would bounce furiously. ",
+               "I slapped your ass and the usted until it turned from a beautiful",
+               " dark to a burning red print of the palm of my hand I pulled",
+               " out before Cummings and u began to beg to have it. ",
+               "You quickly grabbed a pill and swallowed and begs to have it.",
+               " I slapped your ass once more then came deep inside your gut. ",
+               "You sat up on the bed the spread your legs demanding more. ",
+               "Saying they need to keep warm one way or another. ",
+               "You were disappointed I was dried out but u didn't care.",
+               " U still wanted it. U grabbed me with your legs and pulled me towards you.",
+               " You grabbed my semi hard dick and pushed it inside you.",
+               " It slowly got harder with each movement you made and u loved it.",
+               " I was tired out and my dick go so sensitive.",
+               " We turned over and you began to bounce on my dick.",
+               " You leaned in closer and shoved your tits in my mouth once more.",
+               " I gave all the energy I had and bagan to pound your ass we went on for minute",
+               " until we both came at the same time.",
+               " We fell back on the bed both dazed and joyous. ",
+               "We decided wed change the sheets after we rested. ",
+               "We grabbed the blankets and went under both naked ",
+               "and warmed up from each others bodies.",
+               "end",
+            
+            // Add more messages here
+        ],
+        currentMessageIndex: 0
+    },
+
+
+    {
+        textId: 'storyText4',
+        messages: [
+            "Só it was my birthday",
+            " and I was just going about my normal day ",
+            "cuz I ain't really do much on my birthdays.",
+            "  I came home and you were in all black underwear",
+            " on bed and u covered in flour and there was cream ",
+            "on ur lips boobs coochie and ass.",
+            " U were like happy birthday ...",
+            "Then I starting licking it off u then we had fun.",
+            "I  slid it in the u got ribbons and tied us tgth ",
+            "u slid a condom on me and put it in and",
+            " I couldn't come off u cuz we were tied together ",
+            "and ur legs were wrapped around me I came deep inside of you then u untied us",
+            " You then put cream on my dick when nut was still on",
+            " and u sucked everything off.",
+            "-end-",
+            "Gifts",
+            
+            // Add more messages here
+        ],
+        currentMessageIndex: 0
+    },
+
+
+];
+
+
+function changeMessage(box) {
+    const message = box.messages[box.currentMessageIndex];
+    const storyText = document.getElementById(box.textId);
+
+    storyText.style.opacity = 0;
+    setTimeout(() => {
+        if (box.currentMessageIndex < box.messages.length - 1) {
+            box.currentMessageIndex++;
+        } else {
+            box.currentMessageIndex = 0;
+        }
+        storyText.textContent = message;
+        storyText.style.opacity = 1;
+    }, 500); // Adjust the duration of the transition (in milliseconds) as needed
 }
 
-.story-container-parent {
-    display: flex;
-    max-width: 200%;
-    overflow-x: auto; /* Enable horizontal scrolling for the parent container */
-   
-}
+messageBoxes.forEach((box) => {
+    const messageBox = document.getElementById(box.textId);
+    messageBox.addEventListener('click', () => changeMessage(box));
+});
 
-.story-container {
-    min-width: 100%;
-    height: 600px;
-    padding: 20px;
-    margin: 10px;
-    background-color: #FFD3E0;
-    border-radius: 20px;
-    text-align: center;
-    border: 5px solid #FF6B96;
-    transition: margin 0.5s;
-    max-height: 200px; /* Set a maximum height for the container */
-    overflow: auto; /* Enable vertical scrolling for overflow text */
+document.addEventListener("DOMContentLoaded", function () {
+    // Retrieve saved task states from local storage
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key.startsWith("task-")) {
+            const taskIndex = key.replace("task-", "");
+            const task = document.querySelector(`.task:nth-child(${taskIndex})`);
+            if (localStorage.getItem(key) === "clicked") {
+                task.classList.add("clicked");
+            }
+        }
+    }
 
-}
+    // Add click event listeners to tasks
+    const tasks = document.querySelectorAll(".task");
+    tasks.forEach((task, index) => {
+        task.addEventListener("click", () => {
+            task.classList.toggle("clicked");
+            // Save task state to local storage
+            if (task.classList.contains("clicked")) {
+                localStorage.setItem(`task-${index + 1}`, "clicked");
+            } else {
+                localStorage.removeItem(`task-${index + 1}`);
+            }
+        });
+    });
+});
 
-.story-container2 {
-    min-width: 100%;
-    height: 600px;
-    padding: 20px;
-    margin: 10px;
-    background-color: #FFD3E0;
-    border-radius: 20px;
-    text-align: center;
-    border: 5px solid #FF6B96;
-    transition: margin 0.5s;
-    max-height: 200px; /* Set a maximum height for the container */
-    overflow: auto; /* Enable vertical scrolling for overflow text */
-
-}
-
-.story {
-    font-size: 12px;
-    line-height: 1.5;
-    margin: 0;
-    color: #FF4382;
-    opacity: 1;
-    transition: opacity 0.5s;
-    text-align: center;
-}
-.storyNotif {
-    color: #ff0000;
-}
-
-.fixed-text-box {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    background-color: #e94467;
-    color: #fff;
-    padding: 10px;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0);
-    z-index: 999;
-    /* You can adjust the width and height as needed */
-    width: 800px;
-    height: 300px;
-    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-}
-
-.icon {
-        font-size: 60px;
-        position: fixed;
-        top: 0;
-        left: 0;
-        color: #fff;
-
-}
-.navigation ul li a .icon {
-    position: relative;
-    display: block;
-    font-size: 1.5em;
-    text-align: center;
-    transition: 0.5s;
-    color: #fff;
-    transform: translateX(3px); /* Shift right by 3 pixels */
-}
-
-.notification {
-    position: fixed;
-    bottom: 10px;
-    left: 10px;
-    background-color: #ff457d;
-    color: #fff;
-    padding: 10px;
-    font-family: 'Macondo Swash Caps', cursive;
-    border-radius: 5px;
-  }
-  .highlight-word {
-    color: #c368ff; /* Change this to the desired color */
-}
